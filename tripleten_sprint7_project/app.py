4f822999ad7d91080ef85ac5af4dad9c7c5c3700
bohmbox2026 <caracoljal@gmail.com>
2026-07-14 21:20:55 -0600

    make explicit the python version in the requirements.txt file

--- tripleten_sprint7_project/app.py
+++ tripleten_sprint7_project/app.py
@@ -3,7 +3,7 @@ import plotly.graph_objects as go  # Importación de plotly.graph_objects como g
 import streamlit as st
 
 # Leer los datos del archivo CSV
-car_data = pd.read_csv('vehicles_us.csv')
+car_data = pd.read_csv('../data/raw/vehicles_us.csv')
 
 # Crear un botón en la aplicación Streamlit
 hist_button = st.button('Construir histograma')
